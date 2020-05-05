faaf88be9404a23ea48f295df878876e4e3baab9
aziznal <abodenaal@gmail.com>
2020-05-05 16:11:01 +0300

    Adds Structure, Styles, and Assets.
    Adds some images to assets directory.
    Creates a seperate style sheet for each section in top-side.

---
+++ src/app/top-side/links.ts
@@ -0,0 +1,26 @@
+export const links: { name: string; url: string }[] = [
+  {
+    name: 'About Us',
+    url: 'about',
+  },
+  {
+    name: 'Contact',
+    url: 'contact',
+  },
+  {
+    name: 'Missing Delivery',
+    url: 'missing_delivery',
+  },
+  {
+    name: 'FAQ',
+    url: 'faq',
+  },
+  {
+    name: 'Report an Issue',
+    url: 'issue_report',
+  },
+  {
+    name: 'Feedback',
+    url: 'feedback',
+  },
+];
--- src/app/top-side/top-side.component.ts
+++ src/app/top-side/top-side.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { links } from "./links";
 
 @Component({
   selector: 'app-top-side',
@@ -7,14 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopSideComponent implements OnInit {
 
-  links = [
-    'About Us',
-    'Contact',
-    'Missing Delivery',
-    'FAQ',
-    'Report an issue',
-    'Feedback'
-  ]
+  links = links;
 
   constructor() { }
 
