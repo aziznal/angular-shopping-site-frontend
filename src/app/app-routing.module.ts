a6c7c6d41a3f3bfe552866bb4f0cf2881a941bc8
aziznal <abodenaal@gmail.com>
2020-05-13 00:30:41 +0300

    Adds route for Service Test Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,14 @@ import { DefaultPageComponent } from './default-page/default-page.component';
 import { DebugPageComponent } from './debug-page/debug-page.component';
 import { ProductCreatorComponent } from './product-creator/product-creator.component';
 import { TestProductsPageComponent } from './test-products-page/test-products-page.component';
+import { ServiceTestPageComponent } from './service-test-page/service-test-page.component';
 
 const routes: Routes = [
   { path: '', component: DefaultPageComponent },
   { path: 'debug', component: DebugPageComponent },
   { path: 'debug/product-creator', component: ProductCreatorComponent},
-  { path: 'debug/product-test-page', component: TestProductsPageComponent }
+  { path: 'debug/product-test-page', component: TestProductsPageComponent },
+  { path: 'debug/service-test-page', component: ServiceTestPageComponent }
 ]
 
 @NgModule({
