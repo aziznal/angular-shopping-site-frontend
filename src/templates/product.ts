b32728a8a5a8211d801859ac2f84a9f26c5c8f7a
aziznal <abodenaal@gmail.com>
2020-05-14 01:06:06 +0300

    Updates template with final schema

--- src/templates/product.ts
+++ src/templates/product.ts
@@ -1,11 +1,10 @@
 export let Product: {
-  id: string;
-  name: string;
-  img: string;
-  price: string;
-  rating: string;
-  comments: string;
-  specs: string;
-  seller: string;
-  sold: number;
+  id: string;           // Automatically Generated when product is added
+  category: "DESKTOP" | "LAPTOP" | "MONITOR" | "KEYBOARD" | "MOUSE" | "BACKPACK";
+  brand: "SONNY" | "REVOLVO" | "COURSEAIR" | "P&H" | "MAK" | "LOGICHTE" | "AZES";
+  model: string;        // { Random String } - { Two Random Numbers }
+  name: string;         // {Brand} {Model}
+  price: number;        // Integer Value
+  rating: number;       // 2-decimal float value. evaluated out of 5
+  sold: number;         // Integer
 };
