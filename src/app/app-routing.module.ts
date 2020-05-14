9d9727029780e99796060b49a6ee984a90b26750
aziznal <abodenaal@gmail.com>
2020-05-14 15:03:01 +0300

    Adds routes for product-manipulation forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,14 +10,24 @@ import { ProductCreatorComponent } from './product-creator/product-creator.compo
 import { TestProductsPageComponent } from './test-products-page/test-products-page.component';
 import { ServiceTestPageComponent } from './service-test-page/service-test-page.component';
 import { ProductFormsPageComponent } from './product-forms-page/product-forms-page.component';
+import { ProductUpdaterFormComponent } from './product-updater-form/product-updater-form.component';
+import { ProductRemoverFormComponent } from './product-remover-form/product-remover-form.component';
+import { ProductFilterFormComponent } from './product-filter-form/product-filter-form.component';
 
 const routes: Routes = [
   { path: '', component: DefaultPageComponent },
   { path: 'debug', component: DebugPageComponent },
-  { path: 'debug/product-creator', component: ProductCreatorComponent},
   { path: 'debug/product-test-page', component: TestProductsPageComponent },
   { path: 'debug/service-test-page', component: ServiceTestPageComponent },
-  { path: 'debug/product-forms-page', component: ProductFormsPageComponent }
+
+
+  // Product Manipulation Forms' paths:
+  { path: 'debug/product-forms-page', component: ProductFormsPageComponent },
+  { path: 'debug/product-forms-page/creator', component: ProductCreatorComponent },
+  { path: 'debug/product-forms-page/updater', component: ProductUpdaterFormComponent },
+  { path: 'debug/product-forms-page/remover', component: ProductRemoverFormComponent },
+  { path: 'debug/product-forms-page/filter', component: ProductFilterFormComponent },
+
 ]
 
 @NgModule({
